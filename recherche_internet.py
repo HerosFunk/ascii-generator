07351b51e2344f7a8cd9f7ff1ab16e8f970c0eee
Léo Richez <leo.richez59@gmail.com>
2023-02-10 21:15:02 +0100

    debug new version of selenium

--- recherche_internet.py
+++ recherche_internet.py
@@ -20,8 +20,7 @@ import shutil
 def recherche():
     mot_a_rechercher = input("Que voulez-vous recherchez ?  ")
     url = 'https://www.google.com/search?q='+str(mot_a_rechercher)+'&source=lnms&tbm=isch&sa=X&ved=2ahUKEwie44_AnqLpAhUhBWMBHUFGD90Q_AUoAXoECBUQAw&biw=1920&bih=947'
-    
-    driver = webdriver.Chrome()
+
     driver = webdriver.Chrome()
     str1 = driver.capabilities['browserVersion']
     str2 = driver.capabilities['chrome']['chromedriverVersion'].split(' ')[0]
@@ -40,7 +39,7 @@ def url_img(url,driver,mot):
     """
     
     driver.get(url)
-    acceptCookiesUrl = driver.find_element_by_xpath('//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button/div[3]')
+    acceptCookiesUrl = driver.find_element('xpath', '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button/div[3]')
     
     driver.execute_script("arguments[0].click();", WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="yDmH0d"]/c-wiz/div/div/div/div[2]/div[1]/div[3]/div[1]/div[1]/form[2]/div/div/button/div[3]'))))
     
@@ -50,15 +49,15 @@ def url_img(url,driver,mot):
     n = int(input("Quelle image souhaitez-vous prendre?(entier attendu)"))
         
     
-    imgurl = driver.find_element_by_xpath('//*[@id="islrg"]/div[1]/div['+str(n)+']/a[1]')
+    imgurl = driver.find_element('xpath', '//*[@id="islrg"]/div[1]/div['+str(n)+']/a[1]')
     time.sleep(2)                         
                                           
                                                                                 
     imgurl.click()
     time.sleep(2)
     
-    img = driver.find_element_by_xpath('//*[@id="Sva75c"]/div[2]/div/div[2]/div[2]/div[2]/c-wiz/div[2]/div[1]/div[1]/div[2]/div/a/img').get_attribute("src")
-                                                                             
+    img = driver.find_element('xpath', '//*[@id="Sva75c"]/div[2]/div/div[2]/div[2]/div[2]/c-wiz/div/div[1]/div[2]/div[2]/div/a/img').get_attribute("src")
+                                                                     
                                                
                                         
     print(img)
